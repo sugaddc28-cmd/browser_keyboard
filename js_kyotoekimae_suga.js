30bdbe995f14df5c7b4694f8dc168b039a0a356f
S.R. <suga.ddc28@gmail.com>
2026-08-25 14:01:21 +0900

    手動演奏完成

--- js_kyotoekimae_suga.js
+++ js_kyotoekimae_suga.js
@@ -1,2 +1,34 @@
 'use strict'
 
+const notes = ['ド', 'レ', 'ミ', 'ファ', 'ソ', 'ラ', 'シ'];
+const keyboard = document.querySelector(".keyboard");
+const displayNote = document.querySelector(".display_note h1");
+let timerId = null;
+
+// 既存の要素をクリア
+keyboard.innerHTML = '';
+
+notes.forEach(note => {
+  // keyクラスを持つdiv要素を作成
+  const key = document.createElement('div');
+  key.classList.add('key');
+  key.textContent = note;
+
+  // 作った要素を鍵盤に追加
+  keyboard.appendChild(key);
+
+  // クリック時のイベント設定
+  key.addEventListener('click', () => {
+    // すでに動いてるタイマーがあれば削除
+    if(timerId)clearTimeout(timerId);
+
+    // 音名を表示
+    displayNote.textContent=note;
+
+    // 1秒後 (1000ミリ秒後) に表示を消すタイマーをセット
+    timerId = setTimeout(()=>{
+      displayNote.textContent='';
+    },1000);
+  });
+
+});
