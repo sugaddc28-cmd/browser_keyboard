59976e00e8073728983ff0d93360b4818a7320ad
APPEAR <gasugasudesu@gmail.com>
2026-09-09 08:57:13 +0900

    keyの改行をコンパクトに

--- js/keyboard/key.js
+++ js/keyboard/key.js
@@ -21,42 +21,33 @@ export class Key {
 		key.textContent = this.#note.name;
 
 		// 押した時
-		key.addEventListener('pointerdown', () => {
-			NotePlayer.playNote(this.#note);
-		});
+		key.addEventListener('pointerdown', () => NotePlayer.startNote(this.#note));
 
 		// 押したまま要素内に入ってきた時
 		key.addEventListener('pointerenter', (e) => {
 			// 主ボタン（左クリックやタッチ）が押されている状態か判定
-			if (e.buttons === 1) {
-				NotePlayer.playNote(this.#note);
-			}
+			if (e.buttons === 1) {NotePlayer.startNote(this.#note);}
 		});
 
 		// 離した時
-		key.addEventListener('pointerup', () => {
-			NotePlayer.stopNote(this.#note);
-		});
+		key.addEventListener('pointerup', () => NotePlayer.stopNote(this.#note));
 
 		// 押したまま要素の外へ出た時
-		key.addEventListener('pointerleave', () => {
-			NotePlayer.stopNote(this.#note);
-		});
+		key.addEventListener('pointerleave', () => NotePlayer.stopNote(this.#note));
 
 		// タッチ割り込み等でのキャンセル時
-		key.addEventListener('pointercancel', () => {
-			NotePlayer.stopNote(this.#note);
-		});
+		key.addEventListener('pointercancel', () => NotePlayer.stopNote(this.#note));
 
 		return key;
 	}
 
 
-
+	// 押されたときの表示
 	press() {
 		this.#element.classList.add('active');
 	}
 
+	// 
 	release() {
 		this.#element.classList.remove('active');
 	}
