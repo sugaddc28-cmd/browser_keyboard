06e0f6e4c84b1fdf61bf6ec6f2d24972d1f6e95e
APPEAR <gasugasudesu@gmail.com>
2026-09-09 18:03:35 +0900

    未使用elementsの削除

--- js/elements.js
+++ js/elements.js
@@ -2,8 +2,6 @@
 // 巨大になりすぎていたら分割や隠蔽を考えておきたい
 
 export const Elements = Object.freeze({
-	autoPlayButton: document.querySelector(".auto_play_button"),
-	songTitle: document.querySelector(".song_title"),
 	keyboard: document.querySelector(".keyboard"),
 	displayNote: document.querySelector(".display_note h1"),
 	volumeSlider: document.getElementById("volume"),
