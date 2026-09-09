ff24ca8f1c99abc937affa777fdfd0b14498b9eb
APPEAR <gasugasudesu@gmail.com>
2026-09-09 10:56:09 +0900

    Synthから音の通知を実装

--- js/audio/synth.js
+++ js/audio/synth.js
@@ -4,6 +4,32 @@ import { Voice } from "./voice.js";
 export class Synth {
 	static #volume = 1;
 	static #activeVoices = new Map(); // 鳴っているsemitoneを保存
+	static #listeners = new Set(); // 鳴っている音が変わった時、発火する処理
+
+	// 変更を通知するリスナーの登録
+	static addVoiceListener(handlers) {
+		this.#listeners.add(handlers);
+	}
+
+	static removeVoiceListener(handlers) {
+		this.#listeners.delete(handlers);
+	}
+
+	static #notify() {
+		this.#listeners.forEach(callback => callback(Array.from(this.#activeVoices.keys())));
+	}
+
+	// Map 関連
+	// activeVoiceの更新
+	static #setActiveVoice(semitone, voice) {
+		this.#activeVoices.set(semitone, voice);
+		this.#notify();
+	}
+	static #deleteActiveVoice(semitone) {
+		if (this.#activeVoices.delete(semitone)) {
+			this.#notify();
+		}
+	}
 
 	static startNote(semitone,durationMs=null) {
 		// 既存のVoiceが残っている場合は再利用
@@ -15,16 +41,15 @@ export class Synth {
 
 		const frequency = Tuning.getFrequency(semitone);
 
-		
 		const voice = new Voice(
 			frequency,
 			"square",
 			// オシレーター停止時に Map から削除する
-			() => { this.#activeVoices.delete(semitone); }
+			() => { this.#deleteActiveVoice(semitone); }
 		);
 
 		voice.start(this.#volume,durationMs);
-		this.#activeVoices.set(semitone,voice);
+		this.#setActiveVoice(semitone,voice);
 	}
 
 	static stopNote(semitone){
@@ -33,6 +58,4 @@ export class Synth {
 
 		voice.stop();
 	}
-
-	
 }
